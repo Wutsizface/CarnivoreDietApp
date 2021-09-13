abd4c72276b135bd1fb23447d99d2d7d630c8aa4
Jacob Russell <jacob_rssll@yahoo.com>
2021-09-13 09:44:14 -0600

    Initial Commit

---
+++ CarnivoreDiet/CarnivoreDietApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarnivoreDietApp.swift
+//  CarnivoreDiet
+//
+//  Created by Jacob Russell on 9/13/21.
+//
+
+import SwiftUI
+
+@main
+struct CarnivoreDietApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarnivoreDiet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarnivoreDiet
+//
+//  Created by Jacob Russell on 9/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
