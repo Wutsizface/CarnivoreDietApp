9ccdce671420b441fdd7839cde4fc227b73add3a
Jacob Russell <jacob_rssll@yahoo.com>
2022-08-02 15:37:21 -0600

    Built out the TrackingView

--- CarnivoreDiet/TrackingView.swift
+++ CarnivoreDiet/TrackingView.swift
@@ -7,13 +7,56 @@
 
 import SwiftUI
 
+struct TrackerEntry: Identifiable, Codable {
+    var id = UUID()
+    var foodItem: String
+    var amount: String
+}
+
+class Tracking: ObservableObject {
+    @Published var entries = [TrackerEntry]() {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(entries) {
+                UserDefaults.standard.set(encoded, forKey: "Tracker Entries")
+            }
+        }
+    }
+    init() {
+        if let entries = UserDefaults.standard.data(forKey: "Tracker Entries") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([TrackerEntry].self, from: entries) {
+                self.entries = decoded
+                return
+            }
+        }
+        self.entries = []
+    }
+}
+
 struct TrackingView: View {
+    @ObservedObject var tracking = Tracking()
+    @State private var showingAddNote = false
     var body: some View {
         NavigationView {
-            Text ("You may find it helpful to track what you eat on a daily basis and adjust your food amounts as needed.")
-                .navigationBarTitle("Tracker")
+            List {
+                ForEach(tracking.entries) { item in
+                    NavigationLink(item.title, destination:AddNote(note: item, tracking: tracking) )
+                    
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationBarTitle("Tracker")
+            .navigationBarItems(trailing:
+                NavigationLink(
+                    destination: AddTracking(note: nil, tracking: tracking),
+                    label: {
+                    Image(systemName: "plus")
+                })
+            )
         } 
     }
+    
 }
 
 struct TrackingView_Previews: PreviewProvider {
