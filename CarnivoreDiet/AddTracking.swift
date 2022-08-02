cb0a41e2fc830435666664e9b508a11ea6a00864
Jacob Russell <jacob_rssll@yahoo.com>
2022-08-02 09:18:48 -0600

    Added AddTracking view

---
+++ CarnivoreDiet/AddTracking.swift
@@ -0,0 +1,20 @@
+//
+//  AddTracking.swift
+//  CarnivoreDiet
+//
+//  Created by Jacob Russell on 8/2/22.
+//
+
+import SwiftUI
+
+struct AddTracking: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddTracking_Previews: PreviewProvider {
+    static var previews: some View {
+        AddTracking()
+    }
+}
